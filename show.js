8bcf1d9d63d287caece8234b7533b5d7a087245b
João Ramos <poor666@gmail.com>
2011-03-31 00:20:52 +0100

    put show.js with server urls and other small fixs

--- show.js
+++ show.js
@@ -1,3 +1,37 @@
+assetsURL = "http://dl.dropbox.com/u/1205299/present.me/";
+
+
+InflectionJS = {
+	id_suffix: new RegExp('(_ids|_id)$', 'g'),
+	underbar: new RegExp('_', 'g')
+}
+
+if (!String.prototype.humanize)
+{
+    String.prototype.humanize = function(lowFirstLetter)
+    {
+        var str = this.toLowerCase();
+        str = str.replace(InflectionJS.id_suffix, '');
+        str = str.replace(InflectionJS.underbar, ' ');
+        if (!lowFirstLetter)
+        {
+            str = str.capitalize();
+        }
+        return str;
+    };
+}
+
+if (!String.prototype.capitalize)
+{
+    String.prototype.capitalize = function()
+    {
+        var str = this.toLowerCase();
+        str = str.substring(0, 1).toUpperCase() + str.substring(1);
+        return str;
+    };
+}
+
+
 function addScript(url){
 	var s = document.createElement("script");
 	s.type = "text/javascript";
@@ -14,8 +48,8 @@ function addCss(url){
 }
 
 
-addCss("css/show_style.css");
-addScript("inflection.js");
+addCss(assetsURL+"css/show_style.css");
+//addScript(assetsURL+"inflection.js");
 
 function rotatePresentation(arr){
 
@@ -40,25 +74,27 @@ $(document).ajaxError(function(event, request, settings){
   alert("falta os ficheiros");
 });
 
+function checkifLoad(){
+	count = findPage(fileInit).i;
+	rotatePresentation(show);
+}
+
     $(document).ready(function() {
 
-		
-		
 		startPage();
-        
 
 			$.address.externalChange(function(event) {
+
 				_url = event.value.split("/");
 				fileInit = {pasta:_url[1],file:_url[2]}
-				count = findPage(fileInit).i;
-				rotatePresentation(show);
+				
+				setTimeout(checkifLoad, 12);
+				//count = findPage(fileInit).i;
+				//rotatePresentation(show);
+
 			});
 		
 		$.address.change(function(event) {
-		    // do something depending on the event.value property, e.g.
-		    // $('#content').load(event.value + '.xml');
-			 //console.log(event);
-			//alert(event.value);
 
 			_url = event.value.split("/");
 			fileInit = {pasta:_url[1],file:_url[2]}
@@ -84,8 +120,9 @@ $(document).ajaxError(function(event, request, settings){
 
 function startPage(){
 	
-	var targetFolder = "./export/";
 	r = window.location.pathname.split("/index.html")
+	
+	$("body").css('background-image', 'url(export/fundo/fundo.jpg)');
 	
 	$.getJSON('export/data.json', function(data,s,st) {
 	
@@ -96,19 +133,13 @@ function startPage(){
 			}else{
 				
 				show = new Array();
-				
 				$.each(data.pages, function(i,item){
 			        $.each(data.pages[i].states, function(ix,itemx){
 							show.push({page:i,state:ix,pageLabel:data.pages[i].name,stateLabel:data.pages[i].states[ix].state});
 					});
 				});
 				
-				
-				
 			};	
-			
-			//console.log(show)
-			//console.log(findPage(fileInit).i);
 			
 			if(!fileInit.pasta){
 				count = 0;
@@ -116,10 +147,13 @@ function startPage(){
 				count = findPage(fileInit).i;
 			}
 			
+			
+			
 			var vector = result.pages[show[count].page];
 			var created = $("<div class='page'>").appendTo('#main');
 			created.append('<a href="#" rel="#"><img src="export/'+vector.name+'/'+vector.states[show[count].state].state+'"></a>');
-
+			
+			
 			$(document).attr("title", vector.name.humanize() +', '+vector.states[show[count].state].state.humanize().slice(0,-4) + " - " + result.setup.title );
 			$.address.value(vector.name.humanize()+'/'+vector.states[show[count].state].state.humanize().slice(0,-4));
 			count++;
